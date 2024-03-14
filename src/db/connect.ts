aac1d4a8fb67e7c3f3e8aa827f7aaf600bf7726f
Jonathan <70047005+Kirixi@users.noreply.github.com>
2024-03-14 23:42:07 +0800

    feat: GET endpoints implemented

--- src/db/connect.ts
+++ src/db/connect.ts
@@ -4,25 +4,21 @@ import mongoose, { Schema } from "mongoose";
 import { User } from "./interfaces.js";
 // import { createCollections } from "./collections.js";
 dotenv.config();
-
-console.log(process.env.CONNECTION_STRING);
 const mongoConnectionString: any = process.env.CONNECTION_STRING;
 
 export const client = mongoose.connect(mongoConnectionString);
 
 async function connectDb() {
   try {
-    console.log("db successfully connected");
-    // createCollections();
+    await client;
 
-    // console.log(collections)
-    // if (collections.length <= 0) {
-    //     createCollections();
-    // } else {
-    //     console.log("User collection detected");
-    // }
-  } catch (e) {
-    console.log("Error connecting to db");
+    if (mongoose.connection.readyState === 1) {
+      console.log("Database connected successfully.");
+    } else {
+      console.log("Failed to connect to the database");
+    }
+  } catch (e: any) {
+    console.log("Error connecting to db", e.message);
   }
 }
 
--- src/middleware/user.ts
+++ src/middleware/user.ts
@@ -11,7 +11,7 @@ const createUser = async (req: Request, res: Response) => {
       _id: new mongoose.Types.ObjectId(),
       email: req.body.email,
       password: passwordHash,
-      name: req.body.email,
+      name: req.body.username,
     });
 
     const response = await user.save();
@@ -23,13 +23,71 @@ const createUser = async (req: Request, res: Response) => {
   }
 };
 
+const getUserById = async (req: Request, res: Response) => {
+  try {
+    const response = await UserModel.findById(req.params.id);
+    return res.status(200).json(response);
+  } catch (e: any) {
+    console.log(e);
+    return e.message;
+  }
+};
+
+const getUsernamebyEmail = async (req: Request, res: Response) => {
+  try {
+    const response = await UserModel.findOne({ email: req.params.email });
+    return res.status(200).json(response);
+  } catch (e: any) {
+    console.log(e);
+    return e.message;
+  }
+};
+
+const getUserbyUsername = async (req: Request, res: Response) => {
+  try {
+    const response = await UserModel.findOne({ name: req.params.username });
+    return res.status(200).json(response);
+  } catch (e: any) {
+    console.log(e);
+    return e.message;
+  }
+};
+
 const getAllUsers = async (req: Request, res: Response) => {
   try {
     const response = await UserModel.find();
     return res.status(200).json(response);
-  } catch (e) {
+  } catch (e: any) {
     console.log(e);
   }
 };
 
-export default { createUser, getAllUsers };
+const login = async (req: Request, res: Response) => {
+  try {
+    const response = await UserModel.findOne({ email: req.body.email });
+    const passwordHash = await argon2.hash(req.body.password);
+    const userPassword: string | undefined = response?.password;
+
+    if (userPassword) {
+      if (await argon2.verify(passwordHash, userPassword)) {
+        return res.status(200).json(response);
+      } else {
+        return res.status(401).json({ message: "Incorrect password" });
+      }
+    } else {
+      return res.status(401).json({ message: "User is not found" });
+    }
+  } catch (e: any) {
+    console.log(e);
+    return e.message;
+  }
+};
+
+export default {
+  createUser,
+  getAllUsers,
+  getUserById,
+  getUsernamebyEmail,
+  getUserbyUsername,
+  login,
+};
