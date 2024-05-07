f606f4ca72df71efd7ade134036658cfc0d56736
Jonathan <tehyeuhaw@gmail.com>
2024-05-07 17:58:21 +0800

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -14,9 +14,17 @@ provider "aws" {
 resource "aws_instance" "terra-test" {
   ami           = "ami-0ab3794db9457b60a"
   instance_type = "t2.micro"
-  key_name      = "test-key"
+  key_name      = "docker-test"
   tags = {
-    Name = "test"
+    Name = "docker-test-1"
   }
   security_groups = ["launch-wizard-2"]
+  user_data       = <<-EOF
+  #!/bin/bash
+  sudo yum install docker -y
+  sudo service docker start
+  sudo chkconfig docker on
+  sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+  EOF 
 }
