ca5b5cbe60e2605378eb57f1efa33f16168d50fc
Jonathan <tehyeuhaw@gmail.com>
2024-05-14 18:16:45 +0800

    feat: created dynamodb api

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 import { connectDb } from "./db/connect.js";
 import createServer from "./server.js";
-import { main } from "./middleware/DynamoDB/Dynamodb-api.js";
+import { getItemById } from "./middleware/DynamoDB/Dynamodb-api.js";
 
 dotenv.config();
 
@@ -9,16 +9,13 @@ const PORT = 8080;
 
 const app = createServer();
 
-
 app.get("/", (req, res) => {
-  res.status(200).send("hello world!");
+	res.status(200).send("hello world!");
 });
 
 app.listen(PORT, async () => {
-  console.log(`App is listening on port ${PORT}`);
-  
-  await connectDb();
-  main();
+	console.log(`App is listening on port ${PORT}`);
+
+	await connectDb();
+	// await getItemById({ user_id: "9e87e17f-a975-4784-8145-7cd483c13e06" }, "User-dev");
 });
-
-
--- Backend/src/middleware/DynamoDB/Dynamodb-api.ts
+++ Backend/src/middleware/DynamoDB/Dynamodb-api.ts
@@ -1,20 +1,95 @@
-import { PutCommand, DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
+import {
+	PutCommand,
+	GetCommand,
+	UpdateCommand,
+	DeleteCommand,
+	QueryCommand,
+	ScanCommand,
+	DynamoDBDocumentClient,
+} from "@aws-sdk/lib-dynamodb";
 import { client } from "../../db/connect.js";
-import { v4 as uuidv4 } from "uuid";
 
 const docClient = DynamoDBDocumentClient.from(client);
 
-export const main = async () => {
-	const command = new PutCommand({
-		TableName: "User-dev",
-		Item: {
-			user_id: uuidv4(),
-			name: "Shiba Inu",
-			email: "email@gmail.com",
-		},
+const getItemById = async (params: Object, table: string) => {
+	try {
+		const command = new GetCommand({
+			TableName: table,
+			Key: params,
+		});
+
+		const response = await docClient.send(command);
+		console.log(response);
+		return response;
+	} catch (e: any) {
+		throw e;
+	}
+};
+
+const createItem = async (item: Object, table: string) => {
+	try {
+		const command = new PutCommand({
+			TableName: table,
+			Item: item,
+			ReturnValues: "ALL_NEW",
+		});
+
+		const response = await docClient.send(command);
+		console.log(response);
+		return response;
+	} catch (e: any) {
+		throw e;
+	}
+};
+
+const updateItem = async (params: Object, table: string, updateconfig: string, newVal: Object) => {
+	try {
+		const command = new UpdateCommand({
+			TableName: table,
+			Key: params,
+			UpdateExpression: updateconfig,
+			ExpressionAttributeValues: newVal,
+			ReturnValues: "ALL_NEW",
+		});
+
+		const response = await docClient.send(command);
+		console.log(response);
+		return response;
+	} catch (e: any) {
+		throw e;
+	}
+};
+
+const deleteItem = async (params: Object, table: string) => {
+	const command = new DeleteCommand({
+		TableName: table,
+		Key: params,
 	});
 
 	const response = await docClient.send(command);
 	console.log(response);
 	return response;
 };
+
+const queryTable = async (params: Object, table: string, queryExp: string) => {
+	try {
+		const command = new QueryCommand({
+			TableName: table,
+			KeyConditionExpression: queryExp,
+			ExpressionAttributeValues: params,
+			ConsistentRead: true,
+		});
+
+		const response = await docClient.send(command);
+		console.log(response);
+		return response;
+	} catch (e: any) {
+		throw e;
+	}
+};
+
+//TODO
+// const scanTable = async () => {
+
+// }
+export { getItemById, createItem, updateItem, deleteItem, queryTable };
