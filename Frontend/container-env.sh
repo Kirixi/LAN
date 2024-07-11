6ae9f53e3d27945ee230b9203272a5549c445ff5
Jonathan <tehyeuhaw@gmail.com>
2024-07-11 16:04:50 +0800

    Update container-env.sh

--- Frontend/container-env.sh
+++ Frontend/container-env.sh
@@ -5,8 +5,7 @@ ROOT_DIR=/usr/share/nginx/html
 # Replace env vars in files served by NGINX
 for file in $ROOT_DIR/assets/*.js* $ROOT_DIR/index.html;
 do
-  sed -i 's|VITE_API_LINK_PLACEHOLDER|'${VITE_API_LINK}'|g' $file
-  echo ${VITE_API_LINK}
+  sed -i 's|VITE_API_LINK|'${VITE_API_LINK}'|g' $file
 done
 
 # Starting NGINX
