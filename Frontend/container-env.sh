f9322b51181eb593c289087fe1bab28464fcd44b
Jonathan <tehyeuhaw@gmail.com>
2024-07-11 16:17:20 +0800

    test: testing new script for docker

--- Frontend/container-env.sh
+++ Frontend/container-env.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 
-ROOT_DIR=/usr/share/nginx/html
-
-# Replace env vars in files served by NGINX
-for file in $ROOT_DIR/assets/*.js* $ROOT_DIR/index.html;
-do
-  sed -i 's|VITE_API_LINK|'${VITE_API_LINK}'|g' $file
-done
+sed -i "s|VITE_API_LINK|${BACKEND_URL}|g" /usr/share/nginx/html/index.html
 
 # Starting NGINX
-nginx -g 'daemon off;'
+exec "$@"
