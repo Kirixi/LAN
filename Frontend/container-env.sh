cc70962a29988f1e7e93b274a5908118875d80fc
Jonathan <tehyeuhaw@gmail.com>
2024-07-15 13:20:33 +0800

    feat: changed script to accommodate env vars
    - Added env.production file for env var placeholder
    - https://dev.to/sanjayttg/dynamic-environment-variables-for-dockerized-react-apps-5bc5

--- Frontend/container-env.sh
+++ Frontend/container-env.sh
@@ -1,12 +1,11 @@
-#!/bin/sh
-
-ROOT_DIR=/usr/share/nginx/html
-
-# Replace env vars in files served by NGINX
-for file in $ROOT_DIR/js/app.*.js* $ROOT_DIR/index.html;
+for i in $(env | grep MY_APP_)
 do
-  sed -i 's|VITE_API_LINK|'${VITE_API_LINK}'|g' $file
-done
+    key=$(echo $i | cut -d '=' -f 1)
+    value=$(echo $i | cut -d '=' -f 2-)
+    echo $key=$value
+    # sed All files
+    # find /usr/share/nginx/html -type f -exec sed -i "s|${key}|${value}|g" '{}' +
 
-# Starting NGINX
-exec "$@"
+    # sed JS and CSS only
+    find /usr/share/nginx/html -type f \( -name '*.js' -o -name '*.css' \) -exec sed -i "s|${key}|${value}|g" '{}' +
+done
