4de93ff6f7e29de5c2b48b4b4f3c518dd56beaa7
Jonathan <70047005+Kirixi@users.noreply.github.com>
2024-09-21 21:47:29 +0800

    chore: testing env variables

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 import { connectDb } from "./db/connect.js";
 import createServer from "./server.js";
-
+import { AWS_SECRET_KEY, AWS_ACCESS_KEY, S3_BUCKET, REGION } from "./utils/variables.js";
 
 dotenv.config();
 
@@ -9,12 +9,18 @@ const PORT = 8080;
 
 const app = createServer();
 
-
 app.get("/", (req, res) => {
-  res.status(200).send("hello world!");
+	res.status(200).send("hello world!");
 });
 
 app.listen(PORT, async () => {
-  console.log(`App is listening on port ${PORT}`);
-  await connectDb();
+	console.log(`App is listening on port ${PORT}`);
+	console.log("Connection string: ", process.env.CONNECTION_STRING);
+	console.log("Aws access key: ", AWS_ACCESS_KEY);
+
+	console.log("Aws private key: ", AWS_SECRET_KEY);
+	console.log("S3: ", S3_BUCKET);
+	console.log("Region: ", REGION);
+
+	await connectDb();
 });
--- Backend/src/utils/variables.ts
+++ Backend/src/utils/variables.ts
@@ -5,3 +5,4 @@ dotenv.config();
 export const AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY_ID;
 export const AWS_SECRET_KEY = process.env.AWS_SECRET_ACCESS_KEY;
 export const S3_BUCKET = "lan-buckets";
+export const REGION = process.env.REGION;
