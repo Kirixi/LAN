cffefc8b4fdf3ba99667dee3d6e00890523cc6c4
Jonathan <70047005+Kirixi@users.noreply.github.com>
2024-09-21 18:33:04 +0800

    fix: re-encrypt secret

--- Backend/src/utils/variables.ts
+++ Backend/src/utils/variables.ts
@@ -4,4 +4,4 @@ dotenv.config();
 
 export const AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY_ID;
 export const AWS_SECRET_KEY = process.env.AWS_SECRET_ACCESS_KEY;
-export const S3_BUCKET = process.env.S3_BUCKET;
+export const S3_BUCKET = "lan-bucket";
