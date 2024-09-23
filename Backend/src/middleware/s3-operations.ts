18df06f072617f5dc662d8158e9b282e8bb5ead7
Jonathan <70047005+Kirixi@users.noreply.github.com>
2024-09-23 20:44:22 +0800

    fix: removed changes

--- Backend/src/middleware/s3-operations.ts
+++ Backend/src/middleware/s3-operations.ts
@@ -1,6 +1,6 @@
 import * as AWS from "aws-sdk";
 import { AWS_SECRET_KEY, AWS_ACCESS_KEY, S3_BUCKET } from "../utils/variables.js";
-import { PutObjectCommand, S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
+import { S3Client, GetObjectCommand, S3ClientConfig, PutObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { fromEnv } from "@aws-sdk/credential-provider-env";
 import { Response, Request } from "express";
@@ -9,10 +9,15 @@ const validTimer = 3000; //50 Mins
 const validTimerUpload = 500;
 const bucketName = S3_BUCKET.trim();
 
-const s3 = new S3Client({
+const s3Configuration: S3ClientConfig = {
+	credentials: {
+		accessKeyId: AWS_ACCESS_KEY,
+		secretAccessKey: AWS_SECRET_KEY,
+	},
 	region: "ap-northeast-1",
-	credentials: fromEnv(),
-});
+};
+
+const s3 = new S3Client(s3Configuration);
 
 const getURL = async (req: Request, res: Response) => {
 	try {
@@ -26,24 +31,20 @@ const getURL = async (req: Request, res: Response) => {
 };
 
 export const uploadFile = async (imgName: string) => {
-	try {
-		const command = new PutObjectCommand({ Bucket: bucketName, Key: imgName, ContentType: "image/jpeg" });
-		return await getSignedUrl(s3, command, { expiresIn: validTimerUpload });
-	} catch (e: any) {
-		console.log(e);
-		throw e;
-	}
+	const command = new PutObjectCommand({
+		Bucket: bucketName,
+		Key: imgName, // The file name (or path) to be uploaded
+		ContentType: "image/png", // MIME type of the file
+	});
+	const url = await getSignedUrl(s3, command, { expiresIn: 15 * 60 });
+	console.log(url); // Set expiration time for the upload URL
+	return url;
 };
 
 export const getImagePresignUrl = async (imgName: string) => {
-	try {
-		const command = new GetObjectCommand({ Bucket: bucketName, Key: imgName });
-		const response = await getSignedUrl(s3, command, { expiresIn: validTimer });
-		return response;
-	} catch (e: any) {
-		console.log(e);
-		throw e;
-	}
+	const command = new GetObjectCommand({ Bucket: bucketName, Key: imgName });
+	const url = await getSignedUrl(s3, command, { expiresIn: 15 * 60 }); // expires in seconds
+	return url;
 };
 
 export default { getURL };
